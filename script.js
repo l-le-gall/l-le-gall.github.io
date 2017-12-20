6918e1a58bc96912286ef4bec9e7b43c5e078223
l-le-gall <le.gall.laurene.daa@gmail.com>
2017-12-20 16:08:52 +0100

    responsive

---
+++ script.js
@@ -0,0 +1,32 @@
+var boutonhasard = document.querySelector('.boutonhasard');
+
+var pages = [
+  'automne',
+  'baby110_1',
+  'creatures_sociales',
+  'culte_image',
+  'excoffon',
+  'gatsby',
+  'horoscope',
+  'ma_maison',
+  'mem',
+  'moirage',
+  'opera',
+  'reperage_des_allergenes',
+  'sensations',
+  'trames',
+];
+
+var pageCourante = function() {
+  var a = window.location.href.split('projets/')[1];
+  return a.replace('.html', '');
+};
+
+boutonhasard.addEventListener('click', function() {
+  var pageActuelle = pageCourante();
+  var nouvellePage = pages[Math.floor(Math.random() * pages.length)];
+  while (nouvellePage === pageActuelle) {
+    nouvellePage = pages[Math.floor(Math.random() * pages.length)];
+  }
+  window.location = window.location.href.replace(pageActuelle, nouvellePage);
+});
