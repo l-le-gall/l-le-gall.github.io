17848ef5757a49f1d075a969aa5c1c35e1627ddf
l-le-gall <le.gall.laurene.daa@gmail.com>
2018-01-22 01:08:12 +0100

    Nouvelle page d'accueil

---
+++ script_index.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(".slideshow > div:gt(0)").hide();
+    setInterval(function () {
+      $('.slideshow > div:first')
+          .fadeOut(1500)
+          .next()
+          .fadeIn(1500)
+          .end()
+          .appendTo('.slideshow');
+    }, 3000);
+});
